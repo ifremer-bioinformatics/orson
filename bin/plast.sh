af6f63cc37bd64d1366d239d75a5f1de97696271
NOEL <cyril.noel@ifremer.fr>
2021-03-01 14:58:57 +0000

    Fix argument maxhits and maxhsps

--- bin/plast.sh
+++ bin/plast.sh
@@ -29,11 +29,11 @@ module load java/1.8.0_121
 
 # Run PLAST
 if [ "$Q_TYPE" = "n" ]; then
-    CMD="$PLAST_PATH -p plastx -i $QUERY -d $DB -o $CHUNK_OUTPUT_NAME -max-hit-per-query 20 -max-hsp-per-hit 1 -e 1e-3 -a $NCPUS"
+    CMD="$PLAST_PATH -p plastx -i $QUERY -d $DB -o $CHUNK_OUTPUT_NAME -maxhits 20 -maxhsps 1 -e 1e-3 -a $NCPUS"
     echo $CMD > $LOGCMD
     eval $CMD
 else
-    CMD="$PLAST_PATH -p plastp -i $QUERY -d $DB -o $CHUNK_OUTPUT_NAME -max-hit-per-query 20 -max-hsp-per-hit 1 -e 1e-3 -a $NCPUS -outfmt 4"
+    CMD="$PLAST_PATH -p plastp -i $QUERY -d $DB -o $CHUNK_OUTPUT_NAME -maxhits 20 -maxhsps 1 -e 1e-3 -a $NCPUS -outfmt 4"
     echo $CMD > $LOGCMD
     eval $CMD
 fi
