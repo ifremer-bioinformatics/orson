05a75c4064a016ee59d1b21c6b8c220e12a4cf43
Cyril Noël <cyril.noel@ifremer.fr>
2021-03-02 14:29:58 +0000

    add -parse_deflines argument

--- bin/blast.sh
+++ bin/blast.sh
@@ -24,11 +24,11 @@ CHUNK_OUTPUT_NAME=${CHUNK_NAME}_${OUTPUT_NAME}
 
 # Run BLAST
 if [ "$Q_TYPE" = "n" ]; then
-    CMD="blastx -query $QUERY -db $DB -out $CHUNK_OUTPUT_NAME -evalue 1e-3 -outfmt 5 -max_target_seqs 20 -max_hsps 1 -num_threads $NCPUS"
+    CMD="blastx -query $QUERY -db $DB -out $CHUNK_OUTPUT_NAME -evalue 1e-3 -outfmt 5 -max_target_seqs 20 -max_hsps 1 -num_threads $NCPUS -parse_deflines"
     echo $CMD > $LOGCMD
     eval $CMD
 else
-    CMD="blastp -query $QUERY -db $DB -out $CHUNK_OUTPUT_NAME -evalue 1e-3 -outfmt 5 -max_target_seqs 20 -max_hsps 1 -num_threads $NCPUS"
+    CMD="blastp -query $QUERY -db $DB -out $CHUNK_OUTPUT_NAME -evalue 1e-3 -outfmt 5 -max_target_seqs 20 -max_hsps 1 -num_threads $NCPUS -parse_deflines"
     echo $CMD > $LOGCMD
     eval $CMD
 fi
