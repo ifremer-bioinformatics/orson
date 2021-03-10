9ea7347696ee3b7d7d92554dfd073ebef7fb4fd8
Cyril Noël <cyril.noel@ifremer.fr>
2021-03-10 05:12:42 +0000

    remove mkdir because now file output

--- bin/interpro.sh
+++ bin/interpro.sh
@@ -15,8 +15,6 @@ LOGCMD=${args[4]}
 CHUNK_NAME=$(basename ${QUERY%.*})
 CHUNK_OUTPUT_NAME=${CHUNK_NAME}_${OUTPUT_DIRNAME}
 
-mkdir $CHUNK_OUTPUT_DIRNAME
-
 # Run IPRScan
 CMD="interproscan.sh -dp -i $QUERY -t $Q_TYPE -f xml -cpu $NCPUS -o $CHUNK_OUTPUT_NAME -appl Pfam -app ProSiteProfiles -appl CDD -appl TIGRFAM -appl PRINTS -appl SMART -appl SUPERFAMILY -appl Hamap"
 echo $CMD > $LOGCMD
