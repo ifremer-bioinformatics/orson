4c99960498a1bc59f0d56aa884a0176b5a2df361
Cyril Noël <cyril.noel@ifremer.fr>
2021-05-10 12:16:49 +0000

    add download of blast-2.10.0 singularity image

--- bin/get_singularity_images.sh
+++ bin/get_singularity_images.sh
@@ -11,12 +11,12 @@ SING_IMAGES_OK=${args[1]}
 
 container_dir="$BASEDIR/containers"
 
-if [ ! -f "$container_dir/beedeem-4.7.0.sif" ] && [ ! -f "$container_dir/diamond-2.0.9.sif" ] && [ ! -f "$container_dir/iprscan-5.51-85.0.sif" ]
+if [ ! -f "$container_dir/beedeem-4.7.0.sif" ] && [ ! -f "$container_dir/diamond-2.0.9.sif" ] && [ ! -f "$container_dir/iprscan-5.51-85.0.sif" ] && [ ! -f "$container_dir/blast-2.10.0.sif" ]
 then
      wget -r -nc -l1 -nH --cut-dirs=6 -A '*.sif' ftp://ftp.ifremer.fr/ifremer/dataref/bioinfo/sebimer/tools/ORSON/ -P $container_dir
 fi
 
-if [ -f "$container_dir/beedeem-4.7.0.sif" ] && [ -f "$container_dir/diamond-2.0.9.sif" ] && [ -f "$container_dir/iprscan-5.51-85.0.sif" ]
+if [ -f "$container_dir/beedeem-4.7.0.sif" ] && [ -f "$container_dir/diamond-2.0.9.sif" ] && [ -f "$container_dir/iprscan-5.51-85.0.sif" ] && [ -f "$container_dir/blast-2.10.0.sif" ]
 then
      touch $SING_IMAGES_OK
 fi
