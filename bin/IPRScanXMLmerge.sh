bcb1a0023a833da59b28b1ff3130c14ab6ec2dfb
NOEL <cyril.noel@ifremer.fr>
2021-09-08 14:38:20 +0000

    Fix github issue#1

--- bin/IPRScanXMLmerge.sh
+++ bin/IPRScanXMLmerge.sh
@@ -11,14 +11,17 @@ MERGED_XML=${args[0]}
 QUERY_TYPE=${args[1]}
 
 # Run InterProScan XML merging
-CMD="sed -i '1d' *.xml;
-sed -i '\$d' *.xml;
-cat *.xml > $MERGED_XML;
-if [ "$QUERY_TYPE" = "p" ]; then
-	sed -i '1 i\<?xml version=\"1.0\" encoding=\"UTF-8\"?><protein-matches xmlns=\"http://www.ebi.ac.uk/interpro/resources/schemas/interproscan5\" interproscan-version=\"5.51-85.0\">' $MERGED_XML;
+CMD="sed -i '1d' *results.xml;
+sed -i '\$d' *results.xml;
+cat *results.xml > $MERGED_XML;"
+if [ "$QUERY_TYPE" = "p" ]
+then
+	CMD=$CMD . " sed -i '1 i\<?xml version=\"1.0\" encoding=\"UTF-8\"?><protein-matches xmlns=\"http://www.ebi.ac.uk/interpro/resources/schemas/interproscan5\" interproscan-version=\"5.51-85.0\">' $MERGED_XM
+L;
 	echo '</protein-matches>' >> $MERGED_XML"
 else
-	sed -i '1 i\<?xml version=\"1.0\" encoding=\"UTF-8\"?><nucleotide-sequence-matches xmlns=\"http://www.ebi.ac.uk/interpro/resources/schemas/interproscan5\" interproscan-version=\"5.51-85.0\">' $MERGED_XML;
+	CMD=$CMD . " sed -i '1 i\<?xml version=\"1.0\" encoding=\"UTF-8\"?><nucleotide-sequence-matches xmlns=\"http://www.ebi.ac.uk/interpro/resources/schemas/interproscan5\" interproscan-version=\"5.51-85.0\">
+' $MERGED_XML;
         echo '</nucleotide-sequence-matches>' >> $MERGED_XML"
 fi
 eval $CMD
