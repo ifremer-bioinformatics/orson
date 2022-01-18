0e0accec25ea3295d8e47a80bc805b157c83b987
NOEL <cyril.noel@ifremer.fr>
2022-01-18 10:47:53 +0000

    Remove local env loading

--- bin/blast.sh
+++ bin/blast.sh
@@ -16,12 +16,6 @@ LOGCMD=${args[5]}
 CHUNK_NAME=$(basename ${QUERY%.*})
 CHUNK_OUTPUT_NAME=${CHUNK_NAME}_${OUTPUT_NAME}
 
-##################################################################################
-# temporary configuration of the BLAST env in the absence of a singularity image #
-##################################################################################
-. /appli/bioinfo/blast/2.9.0/env.sh
-##################################################################################
-
 # Run BLAST
 if [ "$Q_TYPE" = "n" ]; then
     CMD="blastx -query $QUERY -db $DB -out $CHUNK_OUTPUT_NAME -evalue 1e-3 -outfmt 5 -max_target_seqs 20 -max_hsps 1 -num_threads $NCPUS -parse_deflines"
