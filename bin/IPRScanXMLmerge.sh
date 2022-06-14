f6b08f137430c31fbece9f7c320c12cb203e1437
NOEL <cyril.noel@ifremer.fr>
2022-06-14 15:24:24 +0000

    Fix error in format of InterProScan merged file

--- bin/IPRScanXMLmerge.sh
+++ bin/IPRScanXMLmerge.sh
@@ -10,18 +10,19 @@ args=("$@")
 MERGED_XML=${args[0]}
 QUERY_TYPE=${args[1]}
 
-# Run InterProScan XML merging
-CMD="sed -i '1d' *results.xml;
-sed -i '\$d' *results.xml;
-cat *results.xml > $MERGED_XML;"
+# Format individual InterProScan XML
+sed -i '1d' *results.xml
+sed -i '$d' *results.xml
+
+# Merge individual InterProScan XML
+cat *results.xml > $MERGED_XML
+
+# Reformat merged InterProScan XML
 if [ "$QUERY_TYPE" = "p" ]
 then
-	CMD=$CMD . " sed -i '1 i\<?xml version=\"1.0\" encoding=\"UTF-8\"?><protein-matches xmlns=\"http://www.ebi.ac.uk/interpro/resources/schemas/interproscan5\" interproscan-version=\"5.51-85.0\">' $MERGED_XM
-L;
-	echo '</protein-matches>' >> $MERGED_XML"
+	sed -i '1 i\<?xml version=\"1.0\" encoding=\"UTF-8\"?><protein-matches xmlns=\"http://www.ebi.ac.uk/interpro/resources/schemas/interproscan5\" interproscan-version=\"5.51-85.0\">' $MERGED_XML
+	echo '</protein-matches>' >> $MERGED_XML
 else
-	CMD=$CMD . " sed -i '1 i\<?xml version=\"1.0\" encoding=\"UTF-8\"?><nucleotide-sequence-matches xmlns=\"http://www.ebi.ac.uk/interpro/resources/schemas/interproscan5\" interproscan-version=\"5.51-85.0\">
-' $MERGED_XML;
-        echo '</nucleotide-sequence-matches>' >> $MERGED_XML"
+	sed -i '1 i\<?xml version=\"1.0\" encoding=\"UTF-8\"?><nucleotide-sequence-matches xmlns=\"http://www.ebi.ac.uk/interpro/resources/schemas/interproscan5\" interproscan-version=\"5.51-85.0\">' $MERGED_XML
+	echo '</nucleotide-sequence-matches>' >> $MERGED_XML
 fi
-eval $CMD
