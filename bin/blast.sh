330e32fc1cea7957e0005dd963e06f84dde25cf9
NOEL <cyril.noel@ifremer.fr>
2022-08-01 06:10:56 +0000

    Add restricted search to a specific taxid

--- bin/blast.sh
+++ bin/blast.sh
@@ -12,17 +12,31 @@ Q_TYPE=${args[1]}
 QUERY=${args[2]}
 DB=${args[3]}
 OUTPUT_NAME=${args[4]}
-LOGCMD=${args[5]}
+RESTRICTED_SEARCH=${args[5]}
+RESTRICTED_TAX_ID=${args[6]}
+LOGCMD=${args[7]}
 CHUNK_NAME=$(basename ${QUERY%.*})
 CHUNK_OUTPUT_NAME=${CHUNK_NAME}_${OUTPUT_NAME}
 
 # Run BLAST
 if [ "$Q_TYPE" = "n" ]; then
-    CMD="blastx -query $QUERY -db $DB -out $CHUNK_OUTPUT_NAME -evalue 1e-3 -outfmt 5 -max_target_seqs 20 -max_hsps 1 -num_threads $NCPUS -parse_deflines"
-    echo $CMD > $LOGCMD
-    eval $CMD
+    if [ "$RESTRICTED_SEARCH" = "true" ]; then
+        CMD="blastx -query $QUERY -db $DB -out $CHUNK_OUTPUT_NAME -evalue 1e-3 -outfmt 5 -max_target_seqs 20 -max_hsps 1 -num_threads $NCPUS -parse_deflines -taxids $RESTRICTED_TAX_ID"
+        echo $CMD > $LOGCMD
+        eval $CMD
+    else
+        CMD="blastx -query $QUERY -db $DB -out $CHUNK_OUTPUT_NAME -evalue 1e-3 -outfmt 5 -max_target_seqs 20 -max_hsps 1 -num_threads $NCPUS -parse_deflines"
+        echo $CMD > $LOGCMD
+        eval $CMD
+    fi
 else
-    CMD="blastp -query $QUERY -db $DB -out $CHUNK_OUTPUT_NAME -evalue 1e-3 -outfmt 5 -max_target_seqs 20 -max_hsps 1 -num_threads $NCPUS -parse_deflines"
-    echo $CMD > $LOGCMD
-    eval $CMD
+    if [ "$RESTRICTED_SEARCH" = "true" ]; then
+        CMD="blastp -query $QUERY -db $DB -out $CHUNK_OUTPUT_NAME -evalue 1e-3 -outfmt 5 -max_target_seqs 20 -max_hsps 1 -num_threads $NCPUS -parse_deflines -taxids $RESTRICTED_TAX_ID"
+        echo $CMD > $LOGCMD
+        eval $CMD
+    else
+        CMD="blastp -query $QUERY -db $DB -out $CHUNK_OUTPUT_NAME -evalue 1e-3 -outfmt 5 -max_target_seqs 20 -max_hsps 1 -num_threads $NCPUS -parse_deflines"
+        echo $CMD > $LOGCMD
+        eval $CMD  
+    fi
 fi
