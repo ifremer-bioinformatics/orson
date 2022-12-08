2e36c64891e954f0146173ad172e62faf2fefec0
kamassau <kmassau@sb-roscoff.fr>
2022-12-08 10:22:10 +0100

    Remove chunk names as they are not used anymore (conflict with multiple file outputs)

--- bin/interpro.sh
+++ bin/interpro.sh
@@ -11,8 +11,6 @@ NCPUS=${args[0]}
 QUERY=${args[1]}
 Q_TYPE=${args[2]}
 LOGCMD=${args[3]}
-CHUNK_NAME=$(basename ${QUERY%.*})
-CHUNK_OUTPUT_NAME=${CHUNK_NAME}_${OUTPUT_DIRNAME}
 
 # Run IPRScan
 CMD="interproscan.sh -dp -i $QUERY -t $Q_TYPE -f xml,tsv -cpu $NCPUS  -appl Pfam -app ProSiteProfiles -appl CDD -appl TIGRFAM -appl PRINTS -appl SMART -appl SUPERFAMILY -appl Hamap"
